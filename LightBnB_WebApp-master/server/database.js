9231ec3c6dd038b6ddb356c1d4ce25b21641088c
Jessie Wright <jessie.wright@gmail.com>
2020-04-30 17:29:35 +0000

    added db query for get all reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -67,7 +67,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*,  properties.*, AVG(rating) as average_rating
+  FROM reservations
+  JOIN properties on reservations.property_id = properties.id
+  JOIN property_reviews on reservations.property_id = property_reviews.property_id
+  WHERE end_date < now()::date AND reservations.guest_id = $1
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date DESC
+  LIMIT $2;`,[guest_id, limit])
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
