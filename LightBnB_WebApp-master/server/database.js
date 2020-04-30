e0915a873a11886c59fc4ff3cd18bfe05531e26b
Jessie Wright <jessie.wright@gmail.com>
2020-04-30 17:13:48 +0000

    updated user functions to use db queries

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -16,18 +16,17 @@ const pool = new pg.Pool(config);
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
+
+  const getUserWithEmail = function(email){
+    return pool.query(`SELECT * FROM users WHERE email LIKE $1`,[email])
+    .then(res => {
+      if(res.rows[0]) {
+        return res.rows[0]
+      } else {
+        return null;
+      }
+    })
   }
-  return Promise.resolve(user);
-}
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -36,7 +35,14 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`SELECT * FROM users WHERE id = $1`,[id])
+    .then(res => {
+      if(res.rows[0]) {
+        return res.rows[0]
+      } else {
+        return null;
+      }
+    })
 }
 exports.getUserWithId = getUserWithId;
 
@@ -47,10 +53,9 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`INSERT INTO USERS (name, password, email) 
+                    VALUES ($1,$2,$3) RETURNING *`, [user.name, user.password, user.email])
+                    .then(res => res.rows[0]);
 }
 exports.addUser = addUser;
 
@@ -74,13 +79,6 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
 
   const getAllProperties = function (options, limit = 10) {
     return (
